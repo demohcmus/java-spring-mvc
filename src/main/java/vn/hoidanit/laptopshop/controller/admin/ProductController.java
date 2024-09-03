24b69f78ad8c82cc3f1a1af6c81470632122ae6e
demohcmus <lqngo123@gmail.com>
2024-09-03 09:11:40 +0700

    create a product

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
@@ -1,9 +1,16 @@
 package vn.hoidanit.laptopshop.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.domain.Product;
 
 @Controller
 public class ProductController {
@@ -11,6 +18,25 @@ public class ProductController {
     @GetMapping("/admin/product")
     public String getDashboard() {
         return "admin/product/show";
+    }
+
+    @RequestMapping("/admin/product/create")
+    public String getCreateProductPage(Model model) {
+        model.addAttribute("newProduct", new Product());
+        return "admin/product/create";
+    }
+
+    @PostMapping(value = "/admin/product/create")
+    public String createProductPage(Model model,
+            @ModelAttribute("newProduct") Product product,
+            @RequestParam("productFile") MultipartFile file) {
+        String avatar = this.uploadService.handleSaveUploadFile(file, "avatar");
+        String hashPassword = this.passwordEncoder.encode(hoidanit.getPassword());
+        hoidanit.setAvatar(avatar);
+        hoidanit.setPassword(hashPassword);
+        hoidanit.setRole(this.userService.getRoleByName(hoidanit.getRole().getName()));
+        this.userService.handleSaveUser(hoidanit);
+        return "redirect:/admin/product";
     }
     
 }
